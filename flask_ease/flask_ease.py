ef7cde0cc36ad086f5281e1e58c874c6b0c1de47
aprilahijriyan <hijriyan23@gmail.com>
2021-01-01 14:12:11 -0500

    support for custom endpoints and passing param rules

--- flask_ease/flask_ease.py
+++ flask_ease/flask_ease.py
@@ -1,5 +1,12 @@
-from flask import Blueprint, Flask, request, render_template_string
-from typing import List
+from flask import (
+    Blueprint,
+    Flask,
+    request,
+    render_template_string
+)
+from typing import (
+    List
+)
 from flask_ease.schemas import ResponseModel
 from pydantic.main import ModelMetaclass
 from flask_ease.utils import (
@@ -11,7 +18,7 @@ from flask_ease.utils import (
     generate_auth_scheme,
     HTTPException,
     check_file_validity,
-    extract_files_from_request,
+    extract_files_from_request
 )
 from flask_ease.templates.swagger_ui import html as swagger_html
 from flask_ease.templates.redoc_ui import html as redoc_html
@@ -20,7 +27,7 @@ import json
 from flask_ease.exceptions import messages
 
 
-class FlaskEaseAPI:
+class FlaskEaseAPI():
     def __init__(
         self,
         title: str = "FlaskEase API Docs",
@@ -29,7 +36,7 @@ class FlaskEaseAPI:
         app_version: str = "0.1.0",
         auth_scheme=None,
         import_name=__name__,
-        **kwargs,
+        **kwargs
     ):
         self.blueprint_name = blueprint_name
 
@@ -41,7 +48,9 @@ class FlaskEaseAPI:
         self.app_version = app_version
         self.title = title
         self.endpoints = {}
-        self.open_api = {"openapi": open_api_version}
+        self.open_api = {
+            "openapi": open_api_version
+        }
         self.components = {}
         self.definitions = {}
         self.auth_scheme = auth_scheme
@@ -52,65 +61,78 @@ class FlaskEaseAPI:
             return
         self.open_api = {
             **self.open_api,
-            "info": {"title": self.title, "version": self.app_version},
-            "components": {"schemas": self.components},
+            "info": {
+                "title": self.title,
+                "version": self.app_version
+            },
+            "components": {
+                "schemas": self.components
+            },
             "definitions": self.definitions,
-            "paths": generate_openapi_paths(self.endpoints, self.auth_scheme),
+            "paths": generate_openapi_paths(self.endpoints, self.auth_scheme)
         }
         if self.auth_scheme:
-            self.open_api["components"]["securitySchemes"] = generate_auth_scheme(
-                self.auth_scheme
-            )
+            self.open_api["components"]["securitySchemes"] = \
+                generate_auth_scheme(self.auth_scheme)
 
-        @self.app.route("/docs/openapi.json", methods=["GET"])
+        @self.app.route("/docs/openapi.json", methods=['GET'])
         def get_openapi():
             return self.open_api
 
         @self.app.route("/docs", methods=["GET"])
         def get_swagger_ui():
-            return render_template_string(swagger_html, title=self.title)
+            return render_template_string(
+                swagger_html,
+                title=self.title
+            )
 
         @self.app.route("/redoc", methods=["GET"])
         def get_redoc_ui():
-            return render_template_string(redoc_html, title=self.title)
+            return render_template_string(
+                redoc_html,
+                title=self.title
+            )
 
     def _register(
         self,
         route: str,
         methods: List[str],
-        endpoint: str = None,
         response_model=None,
         tags: List[str] = [],
         auth_required: bool = False,
         responses: dict = {},
+        endpoint: str = None,
         **kwargs
     ):
         def decorate_func(func):
             adjusted_route = route
             if self.blueprint_name and self.app.url_prefix:
-                adjusted_route = self.app.url_prefix + route
+                adjusted_route = self.app.url_prefix+route
 
             doc_details, validations = extract_params(adjusted_route, func)
 
-            (
-                docs_responses,
-                docs_definitions,
-                response_validations,
-            ) = parse_response_model(response_model, responses)
+            docs_responses, docs_definitions, response_validations = \
+                parse_response_model(response_model, responses)
 
             dependencies = extract_dependencies(func)
+            # support for custom endpoints and passing param rules
+            endpoint_view = endpoint or func.__name__
 
             filtered_req_body = {
-                k: v for k, v in doc_details["request_body"]["content"].items() if v
+                k: v
+                for k, v in doc_details["request_body"]["content"].items()
+                if v
             }
             endpoint_doc_details = {
                 "description": func.__doc__.strip() if func.__doc__ else "",
                 "parameters": doc_details["params"],
                 "tags": tags,
-                "requestBody": {"content": filtered_req_body},
-                "endpoint_method": func.__name__,
+                "requestBody": {
+                    "content": filtered_req_body
+                },
+                "endpoint_method": endpoint_view,
                 "responses": docs_responses,
-                "auth_required": auth_required,
+                "auth_required": auth_required
             }
 
             if adjusted_route in self.endpoints.keys():
@@ -123,18 +145,18 @@ class FlaskEaseAPI:
                 }
 
             if (
-                "components" in doc_details.keys()
-                and "schemas" in doc_details["components"].keys()
+                "components" in doc_details.keys() and
+                "schemas" in doc_details["components"].keys()
             ):
                 self.components = {
                     **self.components,
-                    **doc_details["components"]["schemas"],
+                    **doc_details["components"]["schemas"]
                 }
 
             self.definitions = {
                 **self.definitions,
                 **docs_definitions,
-                **doc_details["definitions"],
+                **doc_details["definitions"]
             }
 
             def provide_request(*args, **kwargs):
@@ -162,49 +184,57 @@ class FlaskEaseAPI:
                         elif validations["params"][p]["in"] == "query":
                             query_value = request.args.get(p)
                             if query_value:
-                                kwargs_to_pass[p] = parameter_type(query_value)
+                                kwargs_to_pass[p] = parameter_type(
+                                    query_value
+                                )
 
                     if request_body_key:
-                        request_body_schema = validations["request_body"][
-                            request_body_key
-                        ]["schema"]
+                        request_body_schema = validations[
+                            "request_body"][request_body_key]["schema"]
 
                         if (
                             validations["request_body"][request_body_key]["type"]
-                            == "application/json"
+                                == "application/json"
                         ):
-                            kwargs_to_pass[request_body_key] = request_body_schema(
+                            kwargs_to_pass[
+                                request_body_key
+                            ] = request_body_schema(
                                 **request.json
                             )
-                        elif (
+                        elif(
                             validations["request_body"][request_body_key]["type"]
                             == "file"
                         ):
-                            kwargs_to_pass[request_body_key] = check_file_validity(
-                                request.data, request.mimetype, request_body_schema
+                            kwargs_to_pass[
+                                request_body_key
+                            ] = check_file_validity(
+                                request.data,
+                                request.mimetype,
+                                request_body_schema
                             )
 
                     if request_form_key:
-                        request_form_schema = validations["request_form"][
-                            request_form_key
-                        ]["schema"]
+                        request_form_schema = validations[
+                            "request_form"][request_form_key]["schema"]
                         if (
-                            "properties"
-                            in validations["request_form"][request_form_key]
+                            "properties" in
+                            validations["request_form"][request_form_key]
                         ):
-                            request_form_props = validations["request_form"][
-                                request_form_key
-                            ]["properties"]
+                            request_form_props = validations[
+                                "request_form"][request_form_key]["properties"]
 
                         form_data = {}
                         if (
-                            validations["request_form"][request_form_key]["type"]
-                            == "multipart/form-data"
+                            validations["request_form"][
+                                request_form_key]["type"] == "multipart/form-data"
                         ):
                             for k, v in request_form_props.items():
-                                if v["type"] == ("string", "binary"):
-                                    form_data[k] = extract_files_from_request(
-                                        k, request.files, v["schema"]
+                                if v["type"] == ('string', 'binary'):
+                                    form_data[k] = \
+                                        extract_files_from_request(
+                                        k,
+                                        request.files,
+                                        v["schema"]
                                     )
                                 else:
                                     if k in request.form:
@@ -216,7 +246,9 @@ class FlaskEaseAPI:
                                 request_form_key
                             ] = request_form_schema.schema(**form_data)
                         else:
-                            kwargs_to_pass[request_form_key] = request_form_schema(
+                            kwargs_to_pass[
+                                request_form_key
+                            ] = request_form_schema(
                                 **request.form
                             )
 
@@ -232,7 +264,8 @@ class FlaskEaseAPI:
                             response, response_code = resp
 
                         if response_code in response_validations.keys():
-                            if type(response) is response_validations[response_code]:
+                            if type(response) is response_validations[
+                                    response_code]:
                                 response = response.dict()
 
                             response = response_validations[response_code](
@@ -242,34 +275,36 @@ class FlaskEaseAPI:
                     except Exception as e:
                         logging.exception(e)
                         response = {
-                            "detail": "Something went wrong internally."}
+                            "detail": "Something went wrong internally."
+                        }
                         response_code = 500
                         if type(e) == HTTPException:
-                            response = {"detail": e.detail}
+                            response = {
+                                "detail":   e.detail
+                            }
                             response_code = e.status_code
                 except Exception as e:
                     logging.exception(e)
                     if type(e) == ValueError:
-                        response = {"detail": " ".join(e.args)}
+                        response = {
+                            "detail": " ".join(e.args)
+                        }
                     else:
-                        response = {"detail": json.loads(e.json())}
+                        response = {
+                            "detail": json.loads(e.json())
+                        }
                     response_code = 422
 
                 return response, response_code
 
-            # support for custom endpoints and parameters passed to rules
-            if not endpoint:
-                endpoint = func.__name__
-
-            provide_request.__name__ = endpoint
+            provide_request.__name__ = endpoint_view
             self.app.add_url_rule(
                 rule=route,
-                endpoint=endpoint,
+                endpoint=endpoint_view,
                 view_func=provide_request,
                 methods=methods,
                 **kwargs
             )
-
         return decorate_func
 
     def get(
@@ -279,9 +314,18 @@ class FlaskEaseAPI:
         tags: List[str] = [],
         auth_required: bool = False,
         responses: dict = {},
+        endpoint: str = None,
+        **kwargs
     ):
         return self._register(
-            route, ["GET"], response_model, tags, auth_required, responses
+            route,
+            ["GET"],
+            response_model,
+            tags,
+            auth_required,
+            responses,
+            endpoint,
+            **kwargs
         )
 
     def post(
@@ -291,9 +335,18 @@ class FlaskEaseAPI:
         tags: List[str] = [],
         auth_required: bool = False,
         responses: dict = {},
+        endpoint: str = None,
+        **kwargs
     ):
         return self._register(
-            route, ["POST"], response_model, tags, auth_required, responses
+            route,
+            ["POST"],
+            response_model,
+            tags,
+            auth_required,
+            responses,
+            endpoint,
+            **kwargs
         )
 
     def put(
@@ -303,9 +356,18 @@ class FlaskEaseAPI:
         tags: List[str] = [],
         auth_required: bool = False,
         responses: dict = {},
+        endpoint: str = None,
+        **kwargs
     ):
         return self._register(
-            route, ["PUT"], response_model, tags, auth_required, responses
+            route,
+            ["PUT"],
+            response_model,
+            tags,
+            auth_required,
+            responses,
+            endpoint,
+            **kwargs
         )
 
     def patch(
@@ -315,9 +377,18 @@ class FlaskEaseAPI:
         tags: List[str] = [],
         auth_required: bool = False,
         responses: dict = {},
+        endpoint: str = None,
+        **kwargs
     ):
         return self._register(
-            route, ["PATCH"], response_model, tags, auth_required, responses
+            route,
+            ["PATCH"],
+            response_model,
+            tags,
+            auth_required,
+            responses,
+            endpoint,
+            **kwargs
         )
 
     def delete(
@@ -327,14 +398,33 @@ class FlaskEaseAPI:
         tags: List[str] = [],
         auth_required: bool = False,
         responses: dict = {},
+        endpoint: str = None,
+        **kwargs
     ):
         return self._register(
-            route, ["DELETE"], response_model, tags, auth_required, responses
+            route,
+            ["DELETE"],
+            response_model,
+            tags,
+            auth_required,
+            responses,
+            endpoint,
+            **kwargs
         )
 
     def extend(self, blueprints: list):
         for blueprint in blueprints:
-            self.app.register_blueprint(blueprint.app)
-            self.endpoints = {**self.endpoints, **blueprint.endpoints}
-            self.components = {**self.components, **blueprint.components}
-            self.definitions = {**self.definitions, **blueprint.definitions}
+            self.app.register_blueprint(
+                blueprint.app)
+            self.endpoints = {
+                **self.endpoints,
+                **blueprint.endpoints
+            }
+            self.components = {
+                **self.components,
+                **blueprint.components
+            }
+            self.definitions = {
+                **self.definitions,
+                **blueprint.definitions
+            }
